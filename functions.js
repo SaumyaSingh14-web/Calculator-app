32f052e6cc0a7317e15367380f713b5aec061985
SaumyaSingh14-web <saumyasingh70077@gmail.com>
2023-05-22 06:59:42 -0700

    commit

--- functions.js
+++ functions.js
@@ -1,13 +1,12 @@
 let ioscreen = document.getElementById("ioscreen");
 buttons = document.querySelectorAll("button");
 let screenValue = ""; 
-for (const i of buttons) {
+for(const i of buttons) {
     i.addEventListener("click", (e)=>{
         buttonText = e.target.innerHTML; 
         console.log("button text is ", buttonText);
         if(buttonText == "X"){
             buttonText = "*";
-            screenValue = buttonText;
             ioscreen.value += screenValue;
         }
     })
