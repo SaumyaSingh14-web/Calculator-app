510b6d9466b650153a419e1c451e0747a3928f50
SaumyaSingh14-web <saumyasingh70077@gmail.com>
2023-05-22 06:11:31 -0700

    commit

---
+++ functions.js
@@ -0,0 +1,14 @@
+let ioscreen = document.getElementById("ioscreen");
+buttons = document.querySelectorAll("button");
+let screenValue = ""; 
+for (const i of buttons) {
+    i.addEventListener("click", (e)=>{
+        buttonText = e.target.innerHTML; 
+        console.log("button text is ", buttonText);
+        if(buttonText == "X"){
+            buttonText = "*";
+            screenValue = buttonText;
+            ioscreen.value += screenValue;
+        }
+    })
+}
